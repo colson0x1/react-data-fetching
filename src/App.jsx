f0e8bdb2a2164b80428fb9f618dce7d666188662
Colson <colsoncalifornication@gmail.com>
2024-02-22 12:02:00 +0545

    persist data on backend so the UI persists: send data with POST requests

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
+import { updateUserPlaces } from './http.js';
 
 function App() {
   const selectedPlace = useRef();
@@ -22,7 +23,7 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,6 +33,34 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    // here we don't wanna just update the state but also send our updated
+    // array of places to our backend
+    // we're using after updating the state inside this handleSelectPlace func
+    // here, state update is not  immediately available in this next line of code
+    // instead it will only be available after the component function executed
+    // the next time which is scheduled by this state update there (setUserPlaces)
+    // but this code updateUserPlaces(userPlaces) will still execute before that
+    // happened because its part of that old component function so to say
+    // before it was updated.
+    // So just using our old state (userPlaces) won't work here, instead we should
+    // use our old state and extract the old user places into new array, add the
+    // newly selected place in that new array!!
+    /* updateUserPlaces(userPlaces); // WRONG way */
+    // RIGHT WAY
+    // and this will send this updated array of data to our backend
+    // this operation might take sometime therefore we want to await it before
+    // we continue with the next steps. but await only works in functions that
+    // are decorated in async.
+    // Great thing is, this function handleSelecePlace can be decorated with async
+    // because all we're doing with this function is we're setting it up as
+    // an event listener function in the end, thats triggered whenever we select a place.
+    // when using async await we can also use try catch to wrap this code which might
+    // fail to catch potential errors. so then there after, we can also define some
+    // code that should be executed if this should fail.
+    try {
+      await updateUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {}
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
--- src/http.js
+++ src/http.js
@@ -11,3 +11,24 @@ export async function fetchAvailablePlaces() {
   // after we checked the response is correct, i.e if we didn't throw an error then
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({ places }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  // now look at the response and parse it
+  // .json() also returns promise so we await it
+  const resData = await response.json();
+
+  // if we got error status code
+  if (!response.ok) {
+    throw new Error('Failed to update user data.');
+  }
+
+  return resData.message;
+}
